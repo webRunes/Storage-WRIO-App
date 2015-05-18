14678054e8012c164bdbd19a9e218e102c5fc66b
michbil <denso.ffff@gmail.com>
2015-05-18 20:07:14 +0300

    aws bucket file creation

--- aws.js
+++ aws.js
@@ -3,24 +3,42 @@
  */
 
 var AWS = require('aws-sdk');
-
-var s3 = new AWS.S3();
 var nconf = require("./wrio_nconf.js").init();
-AWS.config.update({accessKeyId: nconf.get("aws:aws_access_key_id"), secretAccessKey: nconf.get("aws:aws_secret_access_key")});
-
-s3.createBucket({Bucket: 'myBucket'}, function() {
-
-    var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
-
-    s3.putObject(params, function(err, data) {
-
-        if (err)
-
-            console.log(err)
-
-        else       console.log("Successfully uploaded data to myBucket/myKey");
-
-    });
-
+var keyid = nconf.get("aws:aws_access_key_id"), secret = nconf.get("aws:aws_secret_access_key");
+AWS.config.update({accessKeyId: keyid, secretAccessKey: secret});
+var s3 = new AWS.S3({
+    params: {Bucket: 'webrunes', Key: 'test'},
+    //endpoint: "http://webrunes.s3-website-us-east-1.amazonaws.com/"
 });
 
+
+var params = {
+    Body:"Hello!",
+    Key:"test/index.html",
+    ACL:'public-read',
+    ContentType:"text/html"
+};
+
+s3.upload(params,function (err,res) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(res);
+});
+
+
+var params = {
+    Bucket: 'wrio' /* required */
+};
+s3.getBucketWebsite(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+});
+
+var params = {
+    Bucket: 'webrunes' /* required */
+};
+s3.getBucketWebsite(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+});
