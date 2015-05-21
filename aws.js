03220a5b4272c34647f3d5aa9ed8b046107c3f0f
michbil <denso.ffff@gmail.com>
2015-05-21 08:18:57 +0300

    added wriologin

--- aws.js
+++ aws.js
@@ -35,10 +35,17 @@ s3.getBucketWebsite(params, function(err, data) {
     else     console.log(data);           // successful response
 });
 
+module.exports.createRandomID = function () {
+
+}
+
+/*
 var params = {
-    Bucket: 'webrunes' /* required */
+    Bucket: 'webrunes'
 };
+
 s3.getBucketWebsite(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else     console.log(data);           // successful response
 });
+*/
--- server.js
+++ server.js
@@ -8,8 +8,69 @@ var aws = require("./aws.js")
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+var wrioLogin = require('./wriologin');
+
+var session = require('express-session');
+var SessionStore = require('express-mysql-session');
+var cookieParser = require('cookie-parser');
+
+
+
+MYSQL_HOST = nconf.get("db:host");
+MYSQL_USER = nconf.get("db:user");
+MYSQL_PASSWORD = nconf.get("db:password");
+MYSQL_DB = nconf.get("db:dbname");
+DOMAIN= nconf.get("db:workdomain");
+
+
+var session_options = {
+    host: MYSQL_HOST,
+    port: 3306,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DB
+}
+
+var cookie_secret = nconf.get("server:cookiesecret");
+var sessionStore = new SessionStore(session_options);
+app.use(cookieParser(cookie_secret));
+app.use(session(
+    {
+
+        secret: cookie_secret,
+        saveUninitialized: true,
+        store: sessionStore,
+        resave: true,
+        cookie: {
+            secure:false,
+            domain:DOMAIN,
+            maxAge: 1000 * 60 * 24 * 30
+        },
+        key: 'sid'
+    }
+));
+
+
 app.get('/', function (request, response) {
 
-    response.render('core.ejs',{});
+
+    console.log(request.sessionID);
+    wrioLogin.loginWithSessionId(request.sessionID,function(err,res) {
+        if (err) {
+            console.log("User not found")
+            response.render('index.ejs',{"error":"Not logged in","user":undefined});
+        } else {
+            response.render('index.ejs',{"user":res});
+            console.log("User found "+res);
+        }
+    })
+});
+
+
+app.get('/logoff',function(request,response) {
+    console.log("Logoff called");
+    response.clearCookie('sid',{'path':'/','domain':DOMAIN});
+    response.redirect('/');
 
 });
