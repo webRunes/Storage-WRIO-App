58c988ab1110a504a48a247683e75ce9e513072c
hbill <denso.ffff@gmail.com>
2015-09-12 11:42:04 +0300

    unit test

--- test/apitest.js
+++ test/apitest.js
@@ -5,7 +5,6 @@ var app = require("../server.js");
 var request = require('supertest');
 var assert = require('assert');
 var should = require('should');
-var Promise = require('es6-promise').Promise;
 
 var stdout_write = process.stdout._write,
     stderr_write = process.stderr._write;
@@ -18,55 +17,9 @@ var Session = require('supertest-session')({
 });
 
 var ready = false;
-var db;
-app.ready = function(database) {
+app.ready = function() {
     ready = true;
-    db = database;
 };
-
-function createDummyUser() {
-    return new Promise(function(resolve,reject) {
-        var webrunesUsers = db.collection('webRunes_Users');
-        var sessions = db.collection('sessions');
-        var dummy = {
-            _id: 0,
-            name: "dummy"
-        };
-        webrunesUsers.updateOne({_id:0},dummy,{upsert:true},function (err) {
-            if (err) {
-                reject();
-                return;
-            }
-
-            var sessionstr = JSON.stringify(
-                {
-                    passport: {
-                        user : 0
-                    }
-                });
-
-            var session = {
-               expires: new Date(Date.now()+1212111),
-               session: sessionstr
-
-            };
-            sessions.insert(session,function (err,records) {
-                if (err) {
-                    reject();
-                    return;
-                }
-                if (!records) {
-                    reject();
-                    return
-                }
-                resolve(records.insertedIds[0]);
-
-            });
-
-
-        });
-    });
-}
 
 describe("API unit tests", function() {
     before(function (done) {
@@ -131,22 +84,5 @@ describe("API unit tests", function() {
 
                 done();
             });
-    });
-    it('should extract data from dummy profile', function(done) {
-        createDummyUser().then(function (sid) {
-            console.log("done, sid:", sid);
-            request(app)
-                .post('/api/get_profile')
-                .set('Cookie',sid)
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end(function (err, res) {
-                    done();
-
-                })
-        }).catch(function(err) {
-            console.log(err);
-            done("error");
-        })
     });
 });
