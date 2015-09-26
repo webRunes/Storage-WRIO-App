96c5a51761b03fc7c53ae969c209279777a4a4d5
Michael Bilenko <denso.ffff@gmail.com>
2015-09-26 21:25:21 +0300

    Fix wrioID not saved for new users

--- storage/route.js
+++ storage/route.js
@@ -122,11 +122,12 @@ module.exports = function (app,db,aws) {
 
             });
         }
-        function saveTempProfile() {
+        function saveTempProfile(user) {
             // try to find wrioID profile from session or from cookie
             console.log("wrioID not found for this profile, making temporary profile persistent....");
+            console.log(request.cookies);
 
-            if (request.cookies.user_profile) {
+/*            if (request.cookies.user_profile) {
                 console.log("Got user_profile cookie");
                 profiles.saveWRIOid(user.userID, request.cookies.user_profile, function (err) {
                     if (err) {
@@ -138,17 +139,19 @@ module.exports = function (app,db,aws) {
 
 
                 })
-            } else {
+            } else {*/
                 profiles.getUserProfile(request.sessionID, function (err, id, profile) {
                     if (err) {
                         throw "Cant get user profile";
                         return;
                     }
-                    profiles.saveWRIOid(user.userID, id, function (err) {
-                        response.send(returnPersistentProfile(json_resp, id, name));
+                    console.log("Got user_profile",profile);
+                    var wrioid = profile._id;
+                    profiles.saveWRIOid(user._id, wrioid.toString(), function (err) {
+                        response.send(returnPersistentProfile(json_resp, id, user.lastName));
                     });
                 });
-            }
+            /*}*/
 
 
         }
@@ -156,12 +159,15 @@ module.exports = function (app,db,aws) {
         wrioLogin.
         getLoggedInUser(request.sessionID).
         then(function (user) {
+            if (!user) {
+                throw new Error("Got no user");
+            }
             if (user.wrioID) {
                 var name = user.lastName;
                 console.log("User found with wrioID=", user.wrioID);
                 response.send(returnPersistentProfile(json_resp, user.wrioID, name));
             } else {
-                saveTempProfile();
+                saveTempProfile(user);
             }
         }).
         catch(function (err) {
